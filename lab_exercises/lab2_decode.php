e2d5529593ca04128ddfb574d7cf425b576dc25a
Jamaica Cañeda <jamaica.caneda0101@gmail.com>
2025-10-21 10:14:09 +0800

    Update lab2_decode.php

--- lab_exercises/lab2_decode.php
+++ lab_exercises/lab2_decode.php
@@ -1,1 +1,11 @@
+<?php
 
+$json_string = '{"name":"Maria","age":21,"email":"maria@example.com"}';
+
+
+$obj = json_decode($json_string);
+$arr = json_decode($json_string, true);
+
+echo "Object: " . $obj->name . "<br>";
+echo "Array: " . $arr["email"];
+?>

21d310676ad53088ed086a415c5ce3baf96df225
Jamaica Cañeda <jamaica.caneda0101@gmail.com>
2025-10-21 10:15:06 +0800

    Update lab3_api.php

--- lab_exercises/lab3_api.php
+++ lab_exercises/lab3_api.php
@@ -1,1 +1,17 @@
+<?php
 
+header('Content-Type: application/json');
+
+
+$user_profile = [
+    "id"     => "1",
+    "name"   => "Theresa",
+    "email"  => "teresa@example.com",
+    "status" => "active"
+];
+
+
+$json_response = json_encode($user_profile);
+echo $json_response;
+
+?>

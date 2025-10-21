ccbe7d205084be8bf290b227a04556ec70712d5f
Jamaica Cañeda <jamaica.caneda0101@gmail.com>
2025-10-21 10:16:44 +0800

    Update lab4_input.php

--- lab_exercises/lab4_input.php
+++ lab_exercises/lab4_input.php
@@ -1,1 +1,27 @@
+<?php
 
+
+$json_data_string = file_get_contents('php://input');
+
+
+if (empty($json_data_string)) {
+    $json_data_string = '{"username": "admin", "password": "1234"}';
+
+}
+$data = json_decode($json_data_string, true);
+
+if ($data === null) {
+    echo "Error: Could not decode JSON data or received empty input.\n";
+} elseif (isset($data['username']) && isset($data['password'])) {
+    
+    $username = $data['username'];
+    $password = $data['password'];
+
+    echo "Username: " . $username . "\n";
+    echo "Password: " . $password . "\n";
+    
+} else {
+    echo "Error: JSON data is missing 'username' or 'password' keys.\n";
+}
+
+?>

88cf35c4b28c607397290c8598c8adc5b1353cb0
Jamaica Cañeda <jamaica.caneda0101@gmail.com>
2025-10-21 10:12:43 +0800

    Update lab1_encode.php

--- lab_exercises/lab1_encode.php
+++ lab_exercises/lab1_encode.php
@@ -1,1 +1,14 @@
+<?php
 
+$student = [
+    "id"    => 101,
+    "name"  => "Neo",
+    "age"   => 21,
+    "course" => "CIT"
+];
+
+
+$json_string = json_encode($student);
+
+echo $json_string;
+?>
